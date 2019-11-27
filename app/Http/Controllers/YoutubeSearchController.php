f49ae6081e28a204a0bbc8afdfdad13949d0f17f
Rajesh Kumar <rajeshkudawala@gmail.com>
2019-11-27 20:35:26 +0100

    Used dependency injection to implement the YoutubeSearch service

--- app/Http/Controllers/YoutubeSearchController.php
+++ app/Http/Controllers/YoutubeSearchController.php
@@ -2,27 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\YoutubeSearchService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class YoutubeSearchController extends Controller
 {
     /**
+     * @var YoutubeSearchService
+     */
+    private $searchService;
+
+    /**
      * Create a new controller instance.
      *
-     * @return void
+     * @param YoutubeSearchService $searchService
      */
-    public function __construct()
+    public function __construct(YoutubeSearchService $searchService)
     {
-        //
+        $this->searchService = $searchService;
     }
 
     /**
      * @param Request $request
      * @return JsonResponse
+     * @throws ValidationException
      */
     public function search(Request $request)
     {
-        return new JsonResponse([]);
+        $this->validate($request, [
+            'query' => 'required|string',
+            'part' => 'sometimes|required|string'
+        ]);
+
+        $data = $request->all();
+
+        if (isset($data['part'])) {
+            return new JsonResponse($this->searchService
+                ->setPart($data['part'])
+                ->search($data['query'])
+            );
+        }
+
+        $response = $this->searchService->search($data['query']);
+
+        return new JsonResponse($response['items']);
     }
 }
